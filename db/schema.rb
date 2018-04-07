b530f891928fd046dfe7ca72b732f0b6f6a61234
Elliot Strong <strong.elliot91@gmail.com>
2018-04-07 10:31:26 -0700

    Finished Comments Feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180405223309) do
+ActiveRecord::Schema.define(version: 20180407155842) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 20180405223309) do
     t.string "catagory"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "author_name"
+    t.text "body"
+    t.integer "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
 end
