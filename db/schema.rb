eacfca15dc4f593f8f317069b560eba7f1d91d86
Elliot Strong <strong.elliot91@gmail.com>
2018-04-09 16:49:55 -0700

    Added Image Support

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180407192235) do
+ActiveRecord::Schema.define(version: 20180409233644) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -21,6 +21,10 @@ ActiveRecord::Schema.define(version: 20180407192235) do
     t.string "catagory"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "comments", force: :cascade do |t|
